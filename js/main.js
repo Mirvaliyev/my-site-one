2c66a200105325dc64383a03faf847f5a2793bd3
Mirvaliyev <mirvaliyev2000@gmail.com>
2021-11-19 14:16:52 +0500

    ha

--- js/main.js
+++ js/main.js
@@ -35,24 +35,39 @@ $(".little_menu").click(
 // section and  navbar
 let section=document.querySelectorAll("section");
 let navitem=document.querySelectorAll(".nav");
+let sal=document.querySelector('.s_a');
 let sectionbottom;
 let sectiontop;
+let sectop;
+let secbottom;
+let about=document.querySelector('.about')
 navitem[0].style.color="yellowgreen";
 navitem[0].classList.add('nav_span');
 function scrollActive(){
-  let sy=window.pageYOffset;
-    for(let i=0;i<section.length;i++){
-      sectiontop=section[i].offsetTop;
-      sectionbottom=section[i].offsetHeight+sectiontop;
-      if(sy>=sectiontop-5&&sy<sectionbottom-5){
-        navitem[i].style.color="yellowgreen";
-        navitem[i].classList.add('nav_span');
-      }
-      else{
-        navitem[i].style.color="rgb(185,185,185)";
-        navitem[i].classList.remove('nav_span');
-      }
+  let sy=window.pageYOffset+$(window).height();
+  let sy2=window.pageYOffset;
+  sectop=section[3].offsetTop+about.offsetHeight;
+  secbottom=section[3].offsetHeight+sectop-about.offsetHeight;
+  if(sy>=sectop-5&&sy<secbottom-5){
+    $('._html div').addClass('add1');
+    $('._css div').addClass('add2');
+    $('._scss div').addClass('add3');
+    $('._bootstrap div').addClass('add4');
+    $('._javascript div').addClass('add5');
+    $('._jquery div').addClass('add6');
+  }
+  for(let i=0;i<section.length;i++){
+    sectiontop=section[i].offsetTop;
+    sectionbottom=section[i].offsetHeight+sectiontop;
+    if(sy2>=sectiontop-5&&sy2<sectionbottom-5){
+      navitem[i].style.color="yellowgreen";
+      navitem[i].classList.add('nav_span');
     }
+    else{
+      navitem[i].style.color="rgb(185,185,185)";
+      navitem[i].classList.remove('nav_span');
+    }
+  }
 }
 window.addEventListener('scroll', scrollActive);
 
@@ -79,14 +94,6 @@ folioright.reveal('.folioright, .right, .adres', { interval: 100 });
 //   return [value];
 // });
 
-let boyi
-let eni
-setTimeout(function(){
-  window.location
-  eni=window.outerWidth
-  boyi=window.outerHeight
-  if(eni<=560){
-    $('.bio').height(boyi);
-    $('.news').height(boyi);
-  }
-},10)
+// about
+
+
